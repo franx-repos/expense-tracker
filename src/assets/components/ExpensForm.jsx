d93e3ca94de949ba33efd1f029106efae4179c8b
hamed mohsen <raahnaward@gmail.com>
2024-05-13 16:25:24 +0200

    modified styling once again

--- src/assets/components/ExpensForm.jsx
+++ src/assets/components/ExpensForm.jsx
@@ -5,27 +5,31 @@ const ExpenseForm = () => {
     <div className="bg-blue-900 p-4"> 
       <div className="px-4 rounded mx-auto max-w-3xl w-full my-32 inputs space-y-6">
         <form className="flex flex-wrap"> 
-          <div className="flex flex-col w-full sm:w-1/2 sm:flex-row sm:items-center sm:mr-6"> 
-            <div className="mb-4 sm:mb-0 sm:mr-6"> 
+          <div className="flex flex-col w-full sm:flex-row sm:items-center sm:mr-6"> 
+            <div className="mb-4 sm:mb-0 sm:mr-6 w-full sm:w-auto"> 
               <label htmlFor="budget">Budget</label>
               <input type="text" id="budget" className="border border-gray-400 w-full" />
             </div>
-            <div>
+            <div className="mb-4 sm:mb-0 sm:mr-6 w-full sm:w-auto"> 
               <label htmlFor="currency">Currency</label>
               <select id="currency" className="border border-gray-400 w-full"></select>
+            </div>
+            <div className="w-full sm:w-auto"> 
+              <label htmlFor="city">City</label>
+              <input type="text" id="city" className="border border-gray-400 w-full" />
             </div>
           </div>
           <div className="flex flex-col w-full"> 
             <div className="flex space-x-6 w-full"> 
-              <div className="flex flex-col w-1/3"> 
+              <div className="flex flex-col w-full sm:w-auto"> 
                 <label htmlFor="description">Description</label>
                 <input type="text" id="description" className="border border-gray-400 w-full" />
               </div>
-              <div className="flex flex-col w-1/3"> 
+              <div className="flex flex-col w-full sm:w-auto"> 
                 <label htmlFor="price">Price</label>
                 <input type="number" id="price" className="border border-gray-400 w-full" />
               </div>
-              <div className="flex flex-col w-1/3"> 
+              <div className="flex flex-col w-full sm:w-auto"> 
                 <label htmlFor="category">Category</label>
                 <select id="category" className="border border-gray-400 w-full"></select>
                 {/* Uncomment and adjust options as needed */}
@@ -43,3 +47,4 @@ const ExpenseForm = () => {
 };
 
 export default ExpenseForm;
+
