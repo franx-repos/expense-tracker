72b793db98eae945f0a4786cfddbe62f85fcc2f5
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-09-19 12:06:50 +0200

    hello again

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
-import "./App.css";
-
 import ExpenseForm from "./assets/components/ExpensForm";
 import ItemList from "./ItemList";
 
 function App() {
   return (
     <>
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
-      <ExpenseForm />
+      <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-800 text-gray-300">
+        {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+        <ExpenseForm
+          currency={""}
+          description={""}
+          price={""}
+          category={""}
+          budget={""}
+        />
 
-      <ItemList />
+        <ItemList />
+      </div>
     </>
   );
 }
